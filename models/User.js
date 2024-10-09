e31e59c20c2f51a695c3b80409c24397e9dfee38
Shamnad-s <75739648+Shamnad-s@users.noreply.github.com>
2024-10-09 11:12:24 +0530

    model commit

--- models/User.js
+++ models/User.js
@@ -4,16 +4,13 @@ const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
-      unique: true,
     },
     password: {
       type: String,
-      required: true,
     },
     role: {
       type: String,
-      required: true,
+
       roles: ["Librarian", "Member"],
     },
     borrowedbooks: [
